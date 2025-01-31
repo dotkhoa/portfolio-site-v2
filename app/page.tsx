123fbebb5cf69503eafea2cc9f4d3431d4420c96
Khoa Dinh <khoadinh2k@gmail.com>
2025-01-30 18:45:29 -0600

    feat: add & implement motion fade transitions

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,30 @@
 export const runtime = "edge";
 
+import * as motion from "motion/react-client"
 import NavBar from "./navbar"
 
 export default function MainPage() {
-  return (
-     <div>
-        <NavBar/>
-        <div className="flex justify-between items-center w-full max-w-screen-md px-4 mx-auto flex-col gap-8 row-start-2 text-xl">
-            {"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
-            {"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
-        </div>
-    </div>
+   return (
+      <div>
+         <NavBar/>
+         <motion.div
+            className="box"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+         >
+            <div className="flex justify-between items-center w-full max-w-screen-md px-4 mx-auto flex-col gap-8 row-start-2 text-xl">
+                <div>
+                    {`Hello. 👋 I am a Software Engineer.
+                My interest in software development started back in 2019 when I was a freshman in college.
+                I enjoy creating things that live on the internet. I take pleasure in learning about new technologies and building things.`}
+                </div>
+                <div>
+                    {` I\'ve had the amazing opportunity to work at a start-up, StaffWise, where I was able to work on a web application that allows hospitals 
+                    to manage their employees and schedules.`}
+                </div>
+            </div>
+        </motion.div>
+     </div>
   );
 }
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,14 +1,22 @@
 export const runtime = "edge";
 
+import * as motion from "motion/react-client"
 import NavBar from "../navbar"
 
 export default function Resume() {
     return (
         <div>
             <NavBar/>
-            <main className="flex justify-between items-center w-full max-w-screen-md px-4 mx-auto flex-col gap-8 row-start-2 text-xl">
-                <div>{"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}</div>
-            </main>
+            <motion.div
+                className="box"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1 }}
+            >
+                <main className="flex justify-between items-center w-full max-w-screen-md px-4 mx-auto flex-col gap-8 row-start-2 text-xl">
+                    <div>{"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}</div>
+                </main>
+            </motion.div>
         </div>
     );
 }
